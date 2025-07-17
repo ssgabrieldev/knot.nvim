e1a81a7cd6b1aaccef25205dc191dbea495ac13f
gabriel <ssgabrieldev@gmail.com>
2025-07-16 23:28:20 -0300

    new: lualine

---
+++ lua/lualine/themes/knot.lua
@@ -0,0 +1,60 @@
+local theme = require("lush_themes.knot")
+
+return {
+  normal = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_01.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_01.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_01.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_01.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_01.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_01.hex },
+  },
+  insert = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+  },
+  terminal = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.red_03.hex },
+  },
+  visual = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+  },
+  replace = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.yellow_03.hex },
+  },
+  command = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.blue_03.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.blue_03.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.blue_03.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.blue_03.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.blue_03.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.blue_03.hex },
+  },
+  inactive = {
+    a = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_03.hex },
+    b = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_03.hex },
+    c = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_03.hex },
+    x = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_03.hex },
+    y = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_03.hex },
+    z = {  bg = theme.theme.StatusLine.bg.hex, fg = theme.colors.gray_03.hex },
+  }
+}
