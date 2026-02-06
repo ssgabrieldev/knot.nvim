9bc63b6357920f1b1aa2b5890886b8665e27f855
gabriel <ssgabrieldev@gmail.com>
2026-02-06 17:51:56 -0300

    new: statusline bg

--- lua/lush_themes/knot.lua
+++ lua/lush_themes/knot.lua
@@ -43,7 +43,7 @@ M.theme = lush(function(injected_functions)
         Comment { fg = M.colors.gray_02 },
         Delimiter { fg = M.colors.gray_03 },
         Keyword { fg = M.colors.red_04 },
-        StatusLine { fg = M.colors.gray_04, bg = M.colors.black_01.li(2) },
+        StatusLine { fg = M.colors.gray_04, bg = M.colors.black_02 },
         DiagnosticInfo { fg = M.colors.blue_04 },
         DiagnosticWarn { fg = M.colors.yellow_04 },
         DiagnosticError { fg = M.colors.red_04 },
@@ -121,7 +121,7 @@ M.theme = lush(function(injected_functions)
         PmenuSel { bg = M.colors.black_03, gui = "NONE" },
 
         -- NvimTree
-        NvimTreeNormal { bg = StatusLine.bg },
+        NvimTreeNormal { bg = M.colors.black_01.li(2) },
         NvimTreeEndOfBuffer { bg = NvimTreeNormal.bg, fg = NvimTreeNormal.bg },
         NvimTreeCursorLine { bg = NvimTreeNormal.bg },
 
